ee066ca70f8946a81159a2c6a470bec7963599c9
MariusHinge <papermarius@gmail.com>
2022-02-04 14:45:34 +0100

    Current Work

---
+++ pseudo_make.sh
@@ -0,0 +1,35 @@
+why3 realize -L theories/ -D coq -T setB.SetGen -o lib/coq/setB
+why3 realize -L theories/ -D coq -T setB.Set -o lib/coq/setB
+why3 realize -L theories/ -D coq -T setB.SetComprehension -o lib/coq/setB
+why3 realize -L theories/ -D coq -T setB.Fset -o lib/coq/setB
+why3 realize -L theories/ -D coq -T setB.FsetNth -o lib/coq/setB
+why3 realize -L theories/ -D coq -T setB.FsetInduction -o lib/coq/setB
+why3 realize -L theories/ -D coq -T setB.Fsetint -o lib/coq/setB
+why3 realize -L theories/ -D coq -T setB.Min -o lib/coq/setB
+why3 realize -L theories/ -D coq -T setB.SetMap -o lib/coq/setB
+
+why3 realize -L theories/ -D coq -T settheoryB.Interval -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.PowerSet -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.Relation -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.Image -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.InverseDomRan -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.Function -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.Identity -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.Sequence -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.IsFinite -o lib/coq/settheoryB
+why3 realize -L theories/ -D coq -T settheoryB.PowerRelation -o lib/coq/settheoryB
+
+why3 realize -L theories/ -D coq -T monoid.Monoid -o lib/coq/monoid
+
+why3 realize -L theories/ -D coq -T RReal.Rdef -o lib/coq/RReal
+why3 realize -L theories/ -D coq -T RReal.Rorder -o lib/coq/RReal
+why3 realize -L theories/ -D coq -T RReal.Rparts -o lib/coq/RReal
+why3 realize -L theories/ -D coq -T RReal.Roperators -o lib/coq/RReal
+why3 realize -L theories/ -D coq -T RReal.Rstructure -o lib/coq/RReal
+
+why3 realize -L theories/ -D coq -T RRealPredicate.Rdef -o lib/coq/test
+why3 realize -L theories/ -D coq -T RRealPredicate.Rorder -o lib/coq/test
+why3 realize -L theories/ -D coq -T RRealPredicate.Rparts -o lib/coq/test
+why3 realize -L theories/ -D coq -T RRealPredicate.Roperators -o lib/coq/test
+why3 realize -L theories/ -D coq -T RRealPredicate.Rstructure -o lib/coq/test
+why3 realize -L theories/ -D coq -T RRealPredicate.THEOREMS -o lib/coq/test
